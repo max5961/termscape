016c9944af941bfc60936ccb3ef0655aa46f5a52
Maxwell Morrissey <mmorrissey92@proton.me>
2025-07-26 20:10:57 -0400

    Work on different approach for token conversion. Write should execute conversion.

--- src/canvas/GridToken.ts
+++ src/canvas/GridToken.ts
@@ -62,6 +62,49 @@ export class GridTokens {
         return this.grid as string[][];
     }
 
+    /**
+     * Convert just a specific segment.  This is necessary for precision re-renders
+     * since if we diff a segment that has already opened an ansi style but yet
+     * to close, then those styles won't be applied in the `process.stdout.write`
+     * operation. Rather than convert the entire grid, convert just a segment of it.
+     */
+    public convertSegment(y: number, segment: (string | IGridToken)[]) {
+        const operations = [] as { x: number; cell: string }[];
+        const xValues = Array.from(this.tokens.get(y)?.values() ?? { length: 0 });
+
+        xValues.forEach((x) => {
+            const token = segment[x] as IGridToken;
+            const left = (segment[x - 1] as IGridToken)?.ansi;
+            const right = (segment[x + 1] as IGridToken)?.ansi;
+
+            // Left and right share same ansi - NO ANSI
+            if (token.ansi === left && token.ansi === right) {
+                operations.push({ x, cell: token.char });
+
+                // Only right shares ansi - OPEN ANSI
+            } else if (token.ansi !== left && token.ansi === right) {
+                operations.push({ x, cell: token.ansi + token.char });
+
+                // Only left shares ansi - CLOSE ANSI
+            } else if (token.ansi === left && token.ansi !== right) {
+                operations.push({ x, cell: token.char + ANSI_RESET });
+
+                // Left and right share no ansi similarities - OPEN AND CLOSE ANSI
+            } else {
+                operations.push({
+                    x,
+                    cell: token.ansi + token.char + ANSI_RESET,
+                });
+            }
+        });
+
+        operations.forEach((op) => {
+            segment[op.x] = op.cell;
+        });
+
+        return segment.join("");
+    }
+
     public pushToken(x: number, y: number): void {
         if (!this.tokens.has(y)) {
             this.tokens.set(y, new Set([x]));
