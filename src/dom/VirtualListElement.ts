cc61dba076d2a6c6a0539f6fcb4c723cb79902a5
Maxwell Morrissey <mmorrissey92@proton.me>
2026-07-18 13:04:26 -0400

    fix: virtual list allow for data changes

--- src/dom/VirtualListElement.ts
+++ src/dom/VirtualListElement.ts
@@ -3,6 +3,7 @@ import { TagNameEnum, VIRTUAL_LIST_ELEMENT } from "../Constants.js";
 import { DomElement } from "./DomElement.js";
 import type { Style } from "./style/Style.js";
 import { IndexBuffer, type IndexBufferOpts } from "./shared/IndexBuffer.js";
+import { logger } from "../shared/Logger.js";
 
 // - override onFocus/onBlur/onShallow... so that children of VirtualList dispatch
 // these handlers at the control of VirtualList and not the FocusNode which is the
@@ -18,12 +19,10 @@ export class VirtualListElement<T = any> extends DomElement {
     private _opts: Required<IndexBufferOpts<T>>;
     private _focusIdx: number;
     private _buffer: IndexBuffer;
-    private _realized: Map<string, DomElement>;
 
     constructor(props: IndexBufferOpts<T>) {
         super();
 
-        this._realized = new Map();
         props.offset ??= 0;
         props.initialIndex ??= 0;
         props.initialIndex = Math.max(0, props.initialIndex);
@@ -36,7 +35,7 @@ export class VirtualListElement<T = any> extends DomElement {
         this.setNextFocus(props.initialIndex);
 
         this._buffer = new IndexBuffer(this._opts);
-        this.reconcile();
+        this.reconcile({ ...this._opts });
 
         this.afterLayout({
             subscribe: true,
@@ -46,7 +45,7 @@ export class VirtualListElement<T = any> extends DomElement {
 
                 if (prevSize !== nextSize) {
                     this.size = nextSize;
-                    this.reconcile();
+                    this.reconcile({ ...this._opts });
                     return true;
                 }
                 return false;
@@ -69,16 +68,15 @@ export class VirtualListElement<T = any> extends DomElement {
     }
 
     public set data(d: T[]) {
-        this._opts.data = d;
-        this.reconcile();
+        this.reconcile({ ...this._opts, data: d });
     }
-    public get data(): Readonly<unknown[]> {
+    public get data(): Readonly<T[]> {
         return this._opts.data;
     }
 
     public set size(n: number) {
         this._opts.size = n;
-        this.reconcile();
+        this.reconcile({ ...this._opts, size: n });
     }
     public get size() {
         return this._opts.size;
@@ -86,12 +84,12 @@ export class VirtualListElement<T = any> extends DomElement {
 
     public focusNext(n: number = 1) {
         this.setNextFocus(n);
-        this.reconcile();
+        this.reconcile({ ...this._opts });
     }
 
     public focusPrev(n: number = 1) {
         this.setNextFocus(-n);
-        this.reconcile();
+        this.reconcile({ ...this._opts });
     }
 
     private setNextFocus(d: number) {
@@ -102,32 +100,83 @@ export class VirtualListElement<T = any> extends DomElement {
         this._focusIdx = n;
     }
 
-    private reconcile() {
+    private reconcile(opts: Required<IndexBufferOpts>) {
         const prev = this._buffer.read();
-        this._buffer.reconcile({ ...this._opts, nextFocusIdx: this._focusIdx });
+        this._buffer.reconcile({ ...opts, nextFocusIdx: this._focusIdx });
         const next = this._buffer.read();
 
+        const prevMap = new Map<string, DomElement>();
+        const nextMap = new Map<string, DomElement>();
+
+        const prevArr = [] as string[];
+        const nextArr = [] as string[];
+
         for (let i = 0; i < prev.length; ++i) {
-            const key = this._opts.getItemKey(this._opts.data[prev[i]]);
-            this._realized.set(key, this._children[i]);
+            const dataIdx = prev[i];
+            const key = this._opts.getItemKey(this._opts.data[dataIdx]);
+            prevMap.set(key, this._children[i]);
+            prevArr.push(key);
+            this._children[i]._becomeProvider(false);
+            this._children[i]._setOwnProvider(false);
         }
 
-        const children = [...this._children];
-        for (const child of children) {
-            this.removeChild(child);
-        }
+        // Need to change opts AFTER using data to find the previous data
+        this._opts = opts;
 
         for (let i = 0; i < next.length; ++i) {
             const dataIdx = next[i];
             const key = this._opts.getItemKey(this._opts.data[dataIdx]);
-            const el =
-                this._realized.get(key) ??
-                this._opts.renderItem(this._opts.data[dataIdx], dataIdx);
+            if (prevMap.has(key)) {
+                nextMap.set(key, prevMap.get(key)!);
+            } else {
+                nextMap.set(
+                    key,
+                    this._opts.renderItem(this._opts.data[dataIdx], dataIdx),
+                );
+            }
+            nextArr.push(key);
 
-            this.appendChild(el);
-            el._focusNode.becomeProvider(dataIdx === this._focusIdx);
-            el._focusNode.setOwnProvider(dataIdx === this._focusIdx);
-            el.style.flexShrink = 0;
+            nextMap.get(key)!._becomeProvider(dataIdx === this._focusIdx);
+            nextMap.get(key)!._setOwnProvider(dataIdx === this._focusIdx);
+        }
+
+        // logger.write({
+        //     prevArr,
+        //     nextArr,
+        // });
+
+        // this.reconcileBufs(prevArr, nextArr, prevMap, nextMap);
+        this.reconcileBufs(prevArr, nextArr, nextMap);
+    }
+
+    private reconcileBufs(
+        prevArr: string[],
+        nextArr: string[],
+        // prevMap: Map<string, DomElement>,
+        nextMap: Map<string, DomElement>,
+    ) {
+        const diffIdxs = [] as number[];
+        // const remove = [] as DomElement[];
+        // const retain = [] as DomElement[];
+        // const add = [] as DomElement[];
+
+        for (let i = 0; i < Math.max(prevArr.length, nextArr.length); ++i) {
+            if (prevArr[i] !== nextArr[i]) {
+                diffIdxs.push(i);
+            }
+        }
+
+        // No tree manipulation needed.
+        if (!diffIdxs.length) {
+            return;
+        } else {
+            const children = [...this._children];
+            for (const child of children) {
+                this.removeChild(child);
+            }
+            for (const key of nextArr) {
+                this.appendChild(nextMap.get(key)!);
+            }
         }
     }
 }
